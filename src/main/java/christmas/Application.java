4263926a20ffdb2a1b44608ca33d351bf4338cf8
0Chord <seaweed.0chord@gmail.com>
2023-11-14 20:17:35 +0900

    feat : app process

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,14 @@
 package christmas;
 
+import christmas.config.ApplicationConfig;
+import christmas.process.Process;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationConfig appConfig = new ApplicationConfig();
+        Process process = appConfig.process();
+        process.play();
     }
+
+
 }
--- src/main/java/christmas/config/ApplicationConfig.java
+++ src/main/java/christmas/config/ApplicationConfig.java
@@ -4,6 +4,20 @@ import christmas.ioadapter.InputAdapter;
 import christmas.ioadapter.OutputAdapter;
 import christmas.ioadapter.impl.StandardInputAdapter;
 import christmas.ioadapter.impl.StandardOutputAdapter;
+import christmas.process.Process;
+import christmas.process.controller.BenefitController;
+import christmas.process.controller.CalendarController;
+import christmas.process.controller.FoodController;
+import christmas.process.controller.OrderController;
+import christmas.process.controller.PrintController;
+import christmas.process.controller.TotalAmountController;
+import christmas.process.impl.OrderProcess;
+import christmas.service.ConvertService;
+import christmas.service.DiscountService;
+import christmas.service.ValidateService;
+import christmas.service.impl.ConvertServiceImpl;
+import christmas.service.impl.DiscountServiceImpl;
+import christmas.service.impl.ValidateServiceImpl;
 
 public class ApplicationConfig {
 
@@ -13,5 +27,46 @@ public class ApplicationConfig {
 
     public OutputAdapter outputAdapter() {
         return new StandardOutputAdapter();
+    }
+
+    public ConvertService convertService() {
+        return new ConvertServiceImpl();
+    }
+
+    public ValidateService validateService() {
+        return new ValidateServiceImpl();
+    }
+
+    public DiscountService discountService() {
+        return new DiscountServiceImpl();
+    }
+
+    public CalendarController calendarController() {
+        return new CalendarController(inputAdapter(), convertService(), validateService(), outputAdapter());
+    }
+
+    public FoodController foodController() {
+        return new FoodController();
+    }
+
+    public OrderController orderController() {
+        return new OrderController(inputAdapter(), convertService(), validateService(), outputAdapter());
+    }
+
+    public PrintController printController() {
+        return new PrintController(outputAdapter(), convertService());
+    }
+
+    public TotalAmountController totalAmountController() {
+        return new TotalAmountController();
+    }
+
+    public BenefitController benefitController() {
+        return new BenefitController(discountService());
+    }
+
+    public Process process() {
+        return new OrderProcess(printController(), orderController(), foodController(), calendarController(),
+                totalAmountController(), benefitController());
     }
 }
